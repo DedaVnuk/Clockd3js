ce208c3d4fbc25fd778c66f077b4b37ed8bc59e7
sumarokov_world <sumarokov.world@gmail.com>
2020-04-25 11:38:11 +0300

    hide variables; fix appending text

--- Clock.js
+++ Clock.js
@@ -2,23 +2,27 @@ const d3 = require("d3");
 
 exports.Clock = function(params = {root_element_id: "clock"}) {
 
-  this.root_element_id = params.root_element_id || "clock";
+  const _root_element_id = params.root_element_id || "clock";
 
-  this.root_element = d3.select(`#${this.root_element_id}`)
-  this.width = parseInt(this.root_element.style("width"));
-  this.height = parseInt(this.root_element.style("height"));
-  this.radius = (this.width > this.height ? this.height / 2 : this.width / 2) - 5;
+  const _root_element = d3.select(`#${_root_element_id}`)
+  let _width = parseInt(_root_element.style("width")) || 200;
+  let _height = parseInt(_root_element.style("height")) || 200;
+  let _radius = (_width > _height ? _height / 2 : _width / 2) - 5;
 
-  this.center_point_radius = 7;
+  let _center_point_radius = 7;
 
-  this.show_main_circle = params.show_main_circle || false;
+  let _show_main_circle = params.show_main_circle || false;
 
-  this.clock = this.root_element.append("svg")
-    .attr("width", this.width)
-    .attr("height", this.height)
+  const _clock = _root_element.append("svg")
+	  .attr("id", "clock-d3js")
+    .attr("width", _width)
+    .attr("height", _height)
+	  .append("g")
 
-  this.digit_clock_as_text = false;
+  let _digit_clock_as_text = false;
 
+
+  const time = d3.timeFormat(params.time_format || "%H:%M:%S")
 
   const scaleDomain = (date, row_type) => {
     let start_of_time = new Date(date.getTime());
@@ -48,42 +52,43 @@ exports.Clock = function(params = {root_element_id: "clock"}) {
    * рисует стрелки на часах
    * @param {number} row_angle угол, на который будет отклонена стрелка
    * @param {string} row_type тип стрелки("hour", "minute", "second")
-   * @return  {undefined}
+   * @return  {void}
    */
   const drawRow = (row_angle, row_type) => {
-    this.clock.select(`path.${row_type}`).remove();
+    _clock.select(`path.${row_type}`).remove();
 
     const row_types = {
-      hour: { outer_radius: this.radius / 2.3, stroke_width: "5px" },
-      minute: { outer_radius: this.radius / 1.5, stroke_width: "3px" },
-      second: { outer_radius: this.radius / 1.2, stroke_width: "1px" },
+      hour: { outer_radius: _radius / 2.3, stroke_width: "5px" },
+      minute: { outer_radius: _radius / 1.5, stroke_width: "3px" },
+      second: { outer_radius: _radius / 1.2, stroke_width: "1px" },
     }
 
-    var row_path = d3.arc()
-      .innerRadius(this.center_point_radius)
+    const row_path = d3.arc()
+      .innerRadius(_center_point_radius)
       .outerRadius(row_types[row_type].outer_radius)
       .startAngle(row_angle)
       .endAngle(row_angle)
 
-    this.clock.append("path")
+    _clock.append("path")
       .classed(`${row_type}`, true)
       .attr("d", row_path())
-      .attr("transform", `translate(${this.width / 2}, ${this.height / 2})`)
+      .attr("transform", `translate(${_width / 2}, ${_height / 2})`)
       .attr("stroke", row_type === "second" ? "red" : "black")
       .attr("stroke-width", row_types[row_type].stroke_width)
 
   }
 
   /**
+	 * добавляет круг в svg
    * @param {string} class_name
    * @param {number} radius
-   * @return  {undefined}
+   * @return  {void}
    */
   const drawCircle = (class_name, radius) => {
-    this.clock.append("circle")
+    _clock.append("circle")
       .classed(class_name, true)
-      .attr("cx", this.width / 2)
-      .attr("cy", this.height / 2)
+      .attr("cx", _width / 2)
+      .attr("cy", _height / 2)
       .attr("r", radius)
       .attr("stroke", "black")
       .attr("fill", "none")
@@ -92,7 +97,7 @@ exports.Clock = function(params = {root_element_id: "clock"}) {
   /**
    * рисует линии, обозначающие часы, минуты, секунды (секундные совпадают с минутными)
    * @param {string} type
-   * @return  {undefined}
+   * @return  {void}
    */
   const drawTicks = type => {
     const tick_params = {
@@ -100,38 +105,65 @@ exports.Clock = function(params = {root_element_id: "clock"}) {
       minute: {angle: 6, stroke_width: "1px"},
     }
 
-    var ticks = d3.arc()
-      .outerRadius(this.radius)
+    const ticks = d3.arc()
+      .outerRadius(_radius)
 
-    this.clock.selectAll(`${type}Tick`)
+    _clock.selectAll(`${type}Tick`)
       .data(d3.range(0, 360, tick_params[type].angle)).enter()
       .append("path")
       .attr("d", (angle, angle_index) => {
-        var inner_radius = this.radius;
+        let inner_radius = _radius;
 
         if(type === "hour") {
           // 12, 3, 6, 9 часов - линию делаем длиннее
           inner_radius -= angle_index % 3 === 0 ? 30 : 20;
         } else {
-          inner_radius = this.radius - 10;
+          inner_radius = _radius - 10;
         }
-        var angle_in_rad = Math.PI * angle / 180
+
+        let angle_in_rad = Math.PI * angle / 180
+
         return ticks
           .innerRadius(inner_radius)
           .startAngle(angle_in_rad)
           .endAngle(angle_in_rad)()
       })
-      .attr("transform", `translate(${this.width / 2}, ${this.height / 2})`)
+      .attr("transform", `translate(${_width / 2}, ${_height / 2})`)
       .attr("stroke", "black")
       .attr("stroke-width", tick_params[type].stroke_width)
   }
 
   /**
+   * добавляет текст в svg
+   * (используется при добавлении цифровых часов и текста с ссылкой)
+   * @param {string} text - текст, который будет добавлен на часы
+	 * @param {string} link - ссылка(href), для текста
+	 * @param {string} link_class - название класса добавляемого текста
+   * @return  {void}
+   */
+  const appendText = (text, link = "", link_class = "digit-clock") => {
+
+    _clock.select(`.${link_class}`).remove();
+
+    _clock.append("a")
+      .classed(link_class, true)
+      .attr("href", link)
+      .attr("target", "_blank")
+      .attr("pointer-events", link === "" ? "none" : "")
+      .append("text")
+      .attr("x", _width / 2)
+      .attr("y", link_class === "digit-clock" ? _radius * 1.5 : _radius / 1.5)
+      .text(text)
+      .attr("text-anchor", "middle")
+      .attr("cursor", "pointer")
+  }
+
+	/**
    * покажет на часах цифровое отображение времени
    * @return  {Clock}
    */
   this.showDigitTime = () => {
-    this.digit_clock_as_text = true;
+    _digit_clock_as_text = true;
 
     return this;
   }
@@ -143,33 +175,32 @@ exports.Clock = function(params = {root_element_id: "clock"}) {
    * @return  {Clock}
    */
   this.setText = (text, link = "") => {
-    this.clock_text = text;
-    this.clock_link = link;
-
-    this.appendText(this.digit_clock_as_text);
+    appendText(text, link, "clock-link");
 
     return this;
   }
 
+	/**
+	 * отрисовывает часы
+	 * @return {void}
+	 */
   this.draw = () => {
-    if(this.show_main_circle) {
-      drawCircle("main-circle", this.radius);
+    if(_show_main_circle) {
+      drawCircle("main-circle", _radius);
     }
-    drawCircle("center-circle", this.center_point_radius);
+    drawCircle("center-circle", _center_point_radius);
 
     ["hour", "minute"].map(time_type => drawTicks(time_type));
 
-    var time = d3.timeFormat("%H:%M:%S")
-
     d3.interval(() => {
-      var date = new Date()
+      let date = new Date()
 
-      if(this.digit_clock_as_text) {
-        this.setText(time(date), this.clock_link);
+      if(_digit_clock_as_text) {
+				appendText(time(date));
       }
 
       ["second", "minute", "hour"].forEach(row_type => {
-        var scale = d3.scaleTime()
+        const scale = d3.scaleTime()
           .domain(scaleDomain(date, row_type))
           .range(scaleRange(row_type));
 
@@ -178,31 +209,6 @@ exports.Clock = function(params = {root_element_id: "clock"}) {
 
     }, 1000)
 
-  }
-
-
-  /**
-   * добавляет текст в svg
-   * (используется при добавлении цифровых часов и текста с ссылкой)
-   * @param {boolean} is_digit_clock
-   * @return  {undefined}
-   */
-  this.appendText = is_digit_clock => {
-    var class_name = is_digit_clock ? "digit-clock" : "clock-link";
-
-    this.clock.select(`.${class_name}`).remove();
-
-    this.clock.append("a")
-      .classed(class_name, true)
-      .attr("href", this.clock_link)
-      .attr("target", "_blank")
-      .attr("pointer-events", is_digit_clock || this.clock_link === "" ? "none" : "")
-      .append("text")
-      .attr("x", this.width / 2)
-      .attr("y", is_digit_clock ? this.radius * 1.5 : this.radius / 1.5)
-      .text(this.clock_text)
-      .attr("text-anchor", "middle")
-      .attr("cursor", "pointer")
   }
 
 }
