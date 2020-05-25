8f7f1fb2ace3f40b12962773f59ac119c947028d
sumarokov_world <sumarokov.world@gmail.com>
2020-05-25 22:45:10 +0300

    time intervals on clock

--- Clock.js
+++ Clock.js
@@ -16,11 +16,14 @@ exports.Clock = function(params = {}) {
   let _show_main_circle = params.show_main_circle !== false;
 
   // определяет будут ли отображаться цифры
-  let _show_hours = params.show_hours !== false; 
+  let _show_hours = params.show_hours !== false;
 
   const _default_hours = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   // массив, определяющий, какие цифры отображать на часах (пустой массив значит, что все цифры будут показыны)
-  let _hours = params.hours || _default_hours; 
+  let _hours = params.hours || _default_hours;
+
+  // интервалы времени, для отображения на циферблате
+  let _times = params.times || [];
 
   const _clock = _root_element.append("svg")
     .attr("id", "clock-d3js")
@@ -32,6 +35,28 @@ exports.Clock = function(params = {}) {
 
 
   const time = d3.timeFormat(params.time_format || "%H:%M:%S")
+
+  /**
+   * преобразует строку вида "HH:mm" в радианы
+   * @param  {String} hours_str - строка, со временем
+   * @return {Number}           - радианы
+   */
+  const hoursToRadians = function(hours_str) {
+    const draw_date = new Date();
+    let [hour, minute, second] = hours_str.split(":");
+
+    draw_date.setHours(hour, minute, second || 0);
+
+    const now = new Date();
+    let [year, month, day] = [now.getFullYear(), now.getMonth()+1, now.getDate()];
+    let now_date = `${year}-${month}-${day}`;
+
+    const scale = d3.scaleTime()
+      .domain([new Date(`${now_date} 00:00:00`), new Date(`${now_date} 24:00:00`)])
+      .range([0, 4*Math.PI])
+
+    return scale(draw_date);
+  }
 
   const scaleDomain = (date, row_type) => {
     let start_of_time = new Date(date.getTime());
@@ -80,7 +105,7 @@ exports.Clock = function(params = {}) {
 
     _clock.append("path")
       .classed(`${row_type}`, true)
-      .attr("d", row_path())
+      .attr("d", row_path)
       .attr("transform", `translate(${_width / 2}, ${_height / 2})`)
       .attr("stroke", row_type === "second" ? "red" : "black")
       .attr("stroke-width", row_types[row_type].stroke_width)
@@ -140,7 +165,9 @@ exports.Clock = function(params = {}) {
       .endAngle(tick => tick.startAngle)
 
     const pie = d3.pie();
-    const ticks_g = _clock.selectAll(`${type}Tick`)
+    const ticks_g = _clock.append("g")
+      .classed(`ticks-${type}`, true)
+      .selectAll(`${type}Tick`)
       .data( pie(new Array(360/tick_params[type].angle).fill(tick_params[type].angle)) ).enter()
       .append("g")
       .classed("g-tick", true)
@@ -170,6 +197,57 @@ exports.Clock = function(params = {}) {
   }
 
   /**
+   * выделяет цветом интервалы на циферблате
+   * @return {void}
+   */
+  const drawTimeArc = function() {
+    _times.forEach(time => {
+
+      const arc = d3.arc()
+        .outerRadius(_radius+1)
+        .innerRadius(_radius-(time.width || 11))
+        .startAngle(hoursToRadians(time.start))
+        .endAngle(hoursToRadians(time.end));
+
+      const time_arc_g = _clock.append("g")
+        .classed(`time-arc-g-${time.name}`, true);
+
+      time_arc_g.append("path")
+        .classed(`time-arc-${time.name || ""}`, true)
+        .attr("d", arc)
+        .attr("fill", time.color || "rgb(150, 18, 150)")
+        .attr("fill-opacity", time.opacity || 0.7)
+        .attr("transform", `translate(${_width / 2}, ${_height / 2})`)
+        .attr("time-name", time.name)
+        .on("mouseenter", function() {
+          if(time.name) {
+            const info_g = _clock.append("g").classed("time-info-g", true)
+
+            info_g.append("rect")
+              .classed("time-info-rect", true)
+              .attr("x", _width/3)
+              .attr("y", _height/3)
+              .attr("width", _radius)
+              .attr("height", 25)
+              .attr("fill", "white")
+
+            info_g.append("text")
+              .classed("time-info-text", true)
+              .attr("x", _width/2)
+              .attr("y", _height/3+20)
+              .style("font", "bold 17px sans-serif")
+              .attr("text-anchor", "middle")
+              .text(time.name)
+          }
+        })
+        .on("mouseleave", function() {
+          _clock.selectAll(".time-info-g").remove();
+        })
+
+    })
+  }
+
+  /**
    * добавляет текст в svg
    * (используется при добавлении цифровых часов и текста с ссылкой)
    * @param {string} text - текст, который будет добавлен на часы
@@ -188,7 +266,7 @@ exports.Clock = function(params = {}) {
       .attr("pointer-events", link === "" ? "none" : "")
       .append("text")
       .attr("x", _width / 2)
-      .attr("y", link_class === "digit-clock" ? _radius * 1.5 : _radius / 1.5)
+      .attr("y", link_class === "digit-clock" ? _radius * 1.5 : _height / 3)
       .text(text)
       .attr("text-anchor", "middle")
       .attr("cursor", "pointer")
@@ -227,6 +305,8 @@ exports.Clock = function(params = {}) {
     drawCircle("center-circle", _center_point_radius);
 
     ["hour", "minute"].map(time_type => drawTicks(time_type));
+
+    drawTimeArc();
 
     d3.interval(() => {
       let date = new Date()
